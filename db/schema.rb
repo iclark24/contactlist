737580cd3873bfc870180fa42ae6c226c1f4a0bd
ian <clarksware24@gmail.com>
2018-11-13 11:41:23 -0700

    new model Notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_182035) do
+ActiveRecord::Schema.define(version: 2018_11_13_183247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,13 @@ ActiveRecord::Schema.define(version: 2018_11_13_182035) do
     t.boolean "living", default: true
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_notes_on_contact_id"
+  end
+
+  add_foreign_key "notes", "contacts"
 end
